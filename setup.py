b626e0caab76e18d240b5247bbe624c05b6fe361
trevalkov <rodrigolf080@gmail.com>
2022-03-24 20:06:46 +0000

    configs

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import find_packages
 
 
 setup(
-    name='ivory_tower',
+    name='automatic_couscous',
     version='0.0.0',
-    description='web pentesting framework',
+    description='deploying scanners with no effort',
     author='trevalkov',
     author_email='trevalkov@protonmail.com',
     url='https://github.com/trevalkov/ivory-tower',
     packages=find_packages(exclude=('tests*', 'testing*')),
-    py_modules=['lib'],
+    py_modules=['configs'],
     entry_points={
         'console_scripts': [
             'mainpy = main.main:main',
